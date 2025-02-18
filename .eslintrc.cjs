a7bf44ae4c0047e95559a7f9feae966ca233332e
Halyna Kravchenko <108605949+minimal005@users.noreply.github.com>
2025-02-18 16:39:40 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    settings: {
+      'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    rules: {},
+  },
+};
